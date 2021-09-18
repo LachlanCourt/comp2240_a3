f8028563095e8278e4bf755a9d06e3fb8f3d2f80
LachlanCourt <80299053+LachlanCourt@users.noreply.github.com>
2021-09-19 08:37:20 +1000

    Implement file read (#2)
    * Implement arg validation
    
    * Implement file read
    
    * Formatting

--- src/A3.java
+++ src/A3.java
@@ -1,8 +1,52 @@
+import java.io.File;
 
 public class A3
 {
     public static void main(String[] args)
     {
-        System.out.println("test");
+        if (!validArgs(args))
+        {
+            System.err.println("Invalid arguments. Usage: <frames:integer> <quantum:integer> <filename:string>+");
+            System.exit(1);
+        }
+        A3 a = new A3();
+        a.run(args);
+    }
+
+    public void run(String[] args)
+    {
+        CPU cpu = new CPU(Integer.valueOf(args[0]), Integer.valueOf(args[1]));
+        cpu.readProcesses(args);
+        // cpu.runFixed();
+        // cpu.runVariable();
+        System.out.println(cpu);
+    }
+
+    private static boolean validArgs(String[] args)
+    {
+        if (args.length < 3)
+        {
+            return false;
+        }
+
+        for (int i = 2; i < args.length; i++)
+        {
+            File f = new File(args[i]);
+            if (!f.exists())
+            {
+                return false;
+            }
+        }
+
+        try
+        {
+            Integer.valueOf(args[0]);
+            Integer.valueOf(args[1]);
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+        return true;
     }
 }
---
+++ src/Process.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Process
+{
+    ArrayList<Integer> pageSequence;
+
+    public Process(ArrayList<Integer> pageSequence_)
+    {
+        pageSequence = pageSequence_;
+    }
+}
