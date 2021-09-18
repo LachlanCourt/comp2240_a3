1d949f51ec00b884219ecefb54aec9d68f24cd5b
LachlanCourt <lachlan.m.court@gmail.com>
2021-09-19 09:06:46 +1000

    Plan run method

--- src/FixedCPU.java
+++ src/FixedCPU.java
@@ -7,6 +7,23 @@ public class FixedCPU extends CPU
 
     @Override
     public void run() {
+        // Loop until complete
+
+        // Move blocked to ready
+        // Take from ready
+        // Check if page exists
+        // if no, block and pass to IO
+        // if yes, loop 3 steps
+        //      increment time
+        //      Move blocked to ready
+        //      Tick IO
+        //      Check if complete, if yes break
+        //      Check if page exists
+        //      if no, block and pass to IO and break
+        //      if yes, continue
+        // if complete, move between queues
+        // if not, put back on ready queue or
+
 
     }
 }
